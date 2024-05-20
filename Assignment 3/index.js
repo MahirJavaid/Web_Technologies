48ead778fc0cbe1525a8d2990999be21136eb849
MahirJavaid <mahirchaudhry180@gmail.com>
2024-05-20 20:58:10 +0500

    Converted my Web Page to express based application

---
+++ Assignment 3/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+let ejsLayouts = require("express-ejs-layouts");
+
+let server = express();
+server.use(express.json());
+server.use(ejsLayouts);
+server.use(express.static("public"));
+server.set("view engine", "ejs");
+
+server.get("/", async (req, res) => {
+    res.render("homepage.ejs");
+});
+
+server.get("/products", async (req, res) => {
+    res.render("products");
+});
+
+server.get("/blog", async (req, res) => {
+    res.render("blog");
+});
+
+server.get("/about-us", async (req, res) => {
+    res.render("about-us");
+});
+
+server.get("/contact-us", async (req, res) => {
+    res.render("contact-us");
+});
+
+server.get("/stories", async (req, res) => {
+    res.render("stories");
+});
+
+server.listen(4000, function() {
+    console.log("Server started listening at port 4000");
+});
